7df1ebd8a3277afacbf9cdd4a4230555a5b30eea
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-08-16 01:16:00 +0300

    StaffController done

---
+++ ApiConsume/WebAPI/Controllers/StaffController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            var values = _staffService.GetAll();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStaff(int id)
+        {
+            var value = _staffService.GetById(id);
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult AddStaff(Staff staff)
+        {
+            _staffService.Add(staff);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff(Staff staff)
+        {
+            _staffService.Delete(staff);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff(Staff staff)
+        {
+            _staffService.Update(staff);
+            return Ok();
+        }
+    }
+}
--- ApiConsume/WebAPI/Program.cs
+++ ApiConsume/WebAPI/Program.cs
@@ -1,3 +1,9 @@
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.EntityFramework;
+using DataAccessLayer.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
 
 var app = builder.Build();
 
