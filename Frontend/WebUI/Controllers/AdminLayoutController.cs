9646c8d17398a9431b3d19768670bc835ec4f329
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-08-17 00:20:57 +0300

    View refactoring with partial structure

--- Frontend/WebUI/Controllers/AdminLayoutController.cs
+++ Frontend/WebUI/Controllers/AdminLayoutController.cs
@@ -12,5 +12,31 @@ namespace WebUI.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PreLoaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavHeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SideBarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
     }
 }
