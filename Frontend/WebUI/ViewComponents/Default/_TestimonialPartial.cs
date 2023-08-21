4587ab4ee7666d5d64bd10165a2dd338c058e8b1
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-08-22 00:16:53 +0300

    Team part done for front

--- Frontend/WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.TestimonialDto;
 
 namespace WebUI.ViewComponents.Default
 {
     public class _TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5126/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
