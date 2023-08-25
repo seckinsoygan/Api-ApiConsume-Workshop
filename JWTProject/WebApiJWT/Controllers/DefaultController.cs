df6e673aff758c674d308d39eaf3c71413b705cb
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-08-25 19:25:49 +0300

    JWT Token Added

---
+++ JWTProject/WebApiJWT/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using WebApiJWT.Models;
+
+namespace WebApiJWT.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+		[HttpGet("[action]")]
+		public IActionResult Test()
+		{
+			return Ok(new CreateToken().TokenCreate());
+		}
+
+		[HttpGet("[action]")]
+		[Microsoft.AspNetCore.Authorization.Authorize]
+		public IActionResult Test2()
+		{
+			return Ok("Hoşgeldiniz");
+		}
+	}
+}
