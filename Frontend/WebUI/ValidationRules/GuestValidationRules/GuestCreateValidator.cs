c80db83fff5060d06233b5f32fe6cfe25ff49cbc
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-09-04 19:56:16 +0300

    Fluent Validation added

---
+++ Frontend/WebUI/ValidationRules/GuestValidationRules/GuestCreateValidator.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace WebUI.ValidationRules.GuestValidationRules
+{
+    public class GuestCreateValidator : AbstractValidator<Guest>
+    {
+
+    }
+}
